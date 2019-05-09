5c542a5591e143c9de773b805b239e8d698a3597
Fernando Zavalia <zavaliafernando@gmail.com>
2019-05-09 00:15:15 -0300

    Small And Normal Students

--- src/views/Students/Browse.jsx
+++ src/views/Students/Browse.jsx
@@ -1,14 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Card,
   CardHeader,
   CardBody,
   Container,
   Row,
-  Button
+  Col,
+  Button,
 } from "reactstrap";
 import { Link } from 'react-router-dom'
 import Header from "components/Headers/Header.jsx";
+
+const students = [
+  {
+    id: 1,
+    name: 'Fernando Zavalia',
+    email: 'zavaliafernando@gmail.com',
+    phoneNumber: '1131952988',
+    dni: '37989060'
+  },
+  {
+    id: 2,
+    name: 'Fernando Zavalia',
+    email: 'zavaliafernando@gmail.com',
+    phoneNumber: '1131952988',
+  },
+  {
+    id: 3,
+    name: 'Fernando Zavalia',
+    email: 'zavaliafernando@gmail.com',
+    phoneNumber: '1131952988',
+    dni: '37989060'
+  },
+  {
+    id: 3,
+    name: 'Fernando Zavalia',
+    email: 'zavaliafernando@gmail.com',
+    phoneNumber: '1131952988',
+    dni: '37989060'
+  },
+  {
+    id: 4,
+    name: 'Fernando Zavalia',
+
+    phoneNumber: '1131952988',
+    dni: '37989060'
+  },
+  {
+    id: 5,
+    name: 'Fernando Zavalia',
+    email: 'zavaliafernando@gmail.com',
+    phoneNumber: '1131952988',
+    dni: '37989060'
+  },
+  {
+    id: 6,
+    name: 'Fernando Zavalia',
+    email: 'zavaliafernando@gmail.com',
+    dni: '37989060'
+  },
+]
 
 export default () => {
   return (
@@ -26,6 +77,8 @@ export default () => {
                 <Link to='/admin/students/create'>
                   <Button color="primary" size="sm">Crear</Button>
                 </Link>
+                <div style={{ height: 20 }}></div>
+                {students.map(student => <Student key={student.id} student={student} />)}
               </CardBody>
             </Card>
           </div>
@@ -34,3 +87,78 @@ export default () => {
     </>
   )
 }
+
+const Student = (props) => {
+
+  const isScreenSmall = () => window.screen.width < 767
+
+  const [hasToUseSmallStudent, setHasToUseSmallStudent] = useState(isScreenSmall())
+
+  useEffect(() => {
+
+    const handleResize = () => setHasToUseSmallStudent(isScreenSmall())
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return hasToUseSmallStudent ? <SmallScreenStudent {...props} /> : <NormalScreenStudent {...props} />
+}
+
+const NormalScreenStudent = ({ student }) =>
+  <Card style={{ marginBottom: 20 }}>
+    <CardBody>
+      <div style={{ display: 'flex', width: '100%' }}>
+        <div style={{ flex: 1 }}>
+          <h2>{student.name}</h2>
+          <div><i>Email:</i> {student.email || '-'}</div>
+          <div><i>Teléfono:</i> {student.phoneNumber || '-'}</div>
+          <div><i>DNI:</i> {student.dni || '-'}</div>
+        </div>
+        <div style={{
+          flex: 1,
+          textAlign: 'right',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-around'
+        }}>
+          <div>
+            <i className="fas fa-pen text-blue" />
+          </div>
+          <div>
+            <i className="fas fa-trash text-red" />
+          </div>
+        </div>
+      </div>
+    </CardBody>
+  </Card>
+
+const SmallScreenStudent = ({ student }) =>
+  <Card style={{ marginBottom: 20 }}>
+    <CardBody>
+      <div style={{ display: 'flex', width: '100%', flexDirection: 'column' }}>
+        <div>
+          <h2>{student.name}</h2>
+          <div><i>Email:</i> {student.email || '-'}</div>
+          <div><i>Teléfono:</i> {student.phoneNumber || '-'}</div>
+          <div><i>DNI:</i> {student.dni || '-'}</div>
+        </div>
+        <div style={{
+          textAlign: 'right',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          marginTop: 20
+        }}>
+          <div style={{ marginRight: 40, cursor: 'pointer' }}>
+            <i className="fas fa-pen text-blue" />
+          </div>
+          <div style={{ cursor: 'pointer' }}>
+            <i className="fas fa-trash text-red" />
+          </div>
+        </div>
+      </div>
+    </CardBody>
+  </Card>
