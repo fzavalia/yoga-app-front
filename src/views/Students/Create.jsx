2461eca7424dc517f011529348d2e4840dd0bfeb
Fernando Zavalia <zavaliafernando@gmail.com>
2019-05-12 11:11:41 -0300

    Refactor Students Form

--- src/views/Students/Create.jsx
+++ src/views/Students/Create.jsx
@@ -1,61 +1,9 @@
 import React from 'react'
-import { Formik } from 'formik'
-import {
-  Form,
-  FormGroup,
-  Input,
-  Label,
-  Button,
-  FormFeedback
-} from 'reactstrap'
+import Form from './Form';
 import api from '../../modules/api';
-import validate from '../../helpers/validate';
-import AdminViewContainer from '../../components/AdminViewContainer';
 
-export default ({ history }) => {
-  return (
-    <Formik
-      initialValues={{
-        name: '',
-        email: '',
-        phoneNumber: '',
-        dni: ''
-      }}
-      validate={values => {
-        const errors = {}
-        if (!values.name) errors.name = 'Requerido'
-        if (values.email && !validate.email(values.email)) errors.email = 'Debe ingresar un email valido'
-        return errors
-      }}
-      onSubmit={(values, actions) => {
-        actions.setSubmitting(true)
-        api.student.create(values)
-          .then(history.goBack)
-          .finally(() => actions.setSubmitting(false))
-      }}
-      render={props =>
-        <AdminViewContainer title='Nuevo Alumno'>
-          <Form>
-            <FormikInput formikProps={props} name='name' label='Nombre' />
-            <FormikInput formikProps={props} name='email' label='Email' />
-            <FormikInput formikProps={props} name='phoneNumber' label='Telefono' inputProps={{ type: 'number' }} />
-            <FormikInput formikProps={props} name='dni' label='DNI' inputProps={{ type: 'number' }} />
-            <Button disabled={props.isSubmitting} onClick={history.goBack}>Cancelar</Button>
-            <Button disabled={props.isSubmitting} onClick={props.handleSubmit} color='info'>Enviar</Button>
-          </Form>
-        </AdminViewContainer>}
-    />
-  )
-}
-
-const FormikInput = ({ formikProps, label, name, inputProps = {} }) =>
-  <FormGroup>
-    <Label>{label}</Label>
-    <Input
-      invalid={formikProps.errors[name]}
-      onChange={e => formikProps.handleChange(name)(e.target.value)}
-      value={formikProps.values[name]}
-      {...inputProps}
-    />
-    <FormFeedback>{formikProps.errors[name]}</FormFeedback>
-  </FormGroup>
+export default ({ history }) =>
+  <Form
+    history={history}
+    submitPromise={values => api.student.create(values)}
+  />
