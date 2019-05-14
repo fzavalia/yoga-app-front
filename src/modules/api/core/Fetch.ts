db3aee19acaf086cebb16da71fe4bb1f62f8b562
Fernando Zavalia <zavaliafernando@gmail.com>
2019-05-13 22:53:50 -0300

    Default Fetch

---
+++ src/modules/api/core/Fetch.ts
@@ -0,0 +1,35 @@
+export enum FetchMethod {
+  GET,
+  POST,
+  PUT,
+  DELETE
+}
+
+export enum FetchOptionsBodyType {
+  JSON,
+  FORM
+}
+
+export interface FetchOptionsBodyArgs {
+  [props: string]: any
+}
+
+export type FetchOptionsHeaders = FetchOptionsBodyArgs
+
+export interface FetchOptionsBody {
+  type: FetchOptionsBodyType,
+  args: { [props: string]: any }
+}
+
+export interface FetchOptions {
+  body?: FetchOptionsBody,
+  headers?: Map<string, string>
+}
+
+export class FetchError {
+  constructor(private status: number, private message: string) {}
+}
+
+export default interface Fetch {
+  fetch: (path: string, method: FetchMethod, options: FetchOptions) => Promise<{}>
+}
