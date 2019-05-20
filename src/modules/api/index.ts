f94ebe3091b2b2d71cb75c1b8953f2a8d720f695
Fernando Zavalia <zavaliafernando@gmail.com>
2019-05-20 01:15:41 -0300

    Add payments request

--- src/modules/api/index.ts
+++ src/modules/api/index.ts
@@ -3,11 +3,21 @@ import ApiQueryStringBuilder from "./impl/ApiQueryStringBuilder";
 import HttpClient from "./core/HttpClient";
 import QueryStringBuilder from "./core/QueryStringBuilder";
 import StudentApiModelRequest from "./apiModelRequests/StudentApiModelRequest";
+import PaymentApiModelRequest from "./apiModelRequests/PaymentApiModelRequest";
 
 const httpClient: HttpClient = new FetchHttpClient("http://localhost:8000");
 const queryStringBuilder: (path: string) => QueryStringBuilder = path =>
   new ApiQueryStringBuilder(path);
 
 export default {
-  student: new StudentApiModelRequest("/students", httpClient, queryStringBuilder)
+  student: new StudentApiModelRequest(
+    "/students",
+    httpClient,
+    queryStringBuilder
+  ),
+  payment: new PaymentApiModelRequest(
+    "/payments",
+    httpClient,
+    queryStringBuilder
+  )
 };
