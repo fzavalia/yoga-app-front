56b8c547f13b2717d840c106d8c2dab4018f1acd
Fernando Zavalia <zavaliafernando@gmail.com>
2019-05-23 11:53:00 -0300

    FormViewComponent

---
+++ src/components/FormView.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "./Button";
+import helpers from "../helpers";
+
+export default (props: {
+  title: string;
+  children: any;
+  onCancel: () => void;
+  onSubmit: () => void;
+}) => {
+  return (
+    <>
+      <Title>{props.title}</Title>
+      <form onSubmit={e => e.preventDefault()}>
+        {props.children}
+        <Button
+          className="sm"
+          colors={{ main: helpers.color.secondary }}
+          onClick={props.onCancel}
+        >
+          Cancelar
+        </Button>
+        <Button
+          style={{ float: "right" }}
+          className="sm"
+          colors={{ main: helpers.color.secondary }}
+          onClick={props.onSubmit}
+        >
+          Enviar
+        </Button>
+      </form>
+    </>
+  );
+};
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+export const InputTitle = styled.div`
+  color: ${helpers.color.secondary}
+  font-size: 0.8rem;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  border: unset;
+  font-size: 1rem;
+  height: 2rem;
+  border-bottom: solid 1px ${helpers.color.secondary};
+  margin-bottom: 1rem;
+  transition: border-bottom-color 500ms;
+  &.invalid {
+    border-bottom-color: ${helpers.color.danger}
+  }
+  &:focus {
+    outline: unset
+    border-bottom-color: ${helpers.color.secondaryLight};
+  }
+`;
--- src/views/students/Form/FormView.tsx
+++ src/views/students/Form/FormView.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-import helpers from "../../../helpers";
-import Button from "../../../components/Button";
+import FormView, { InputTitle, Input } from "../../../components/FormView";
 
 export default (props: {
   title: string;
@@ -14,69 +12,29 @@ export default (props: {
   onSubmit: () => void;
 }) => {
   return (
-    <>
-      <Title>{props.title}</Title>
-      <form onSubmit={e => e.preventDefault()}>
-        <InputTitle>Nombre</InputTitle>
-        <Input name="name" value={props.name} onChange={props.onChange} />
-        <InputTitle>Email</InputTitle>
-        <Input
-          name="email"
-          type="email"
-          value={props.email}
-          onChange={props.onChange}
-        />
-        <InputTitle>Telefono</InputTitle>
-        <Input
-          name="phoneNumber"
-          type="tel"
-          value={props.phoneNumber}
-          onChange={props.onChange}
-        />
-        <InputTitle>DNI</InputTitle>
-        <Input name="dni" value={props.dni} onChange={props.onChange} />
-        <Button
-          className="sm"
-          colors={{ main: helpers.color.secondary }}
-          onClick={props.onCancel}
-        >
-          Cancelar
-        </Button>
-        <Button
-          style={{ float: "right" }}
-          className="sm"
-          colors={{ main: helpers.color.secondary }}
-          onClick={props.onSubmit}
-        >
-          Enviar
-        </Button>
-      </form>
-    </>
+    <FormView
+      title="Crear Alumno"
+      onCancel={props.onCancel}
+      onSubmit={props.onSubmit}
+    >
+      <InputTitle>Nombre</InputTitle>
+      <Input name="name" value={props.name} onChange={props.onChange} />
+      <InputTitle>Email</InputTitle>
+      <Input
+        name="email"
+        type="email"
+        value={props.email}
+        onChange={props.onChange}
+      />
+      <InputTitle>Telefono</InputTitle>
+      <Input
+        name="phoneNumber"
+        type="tel"
+        value={props.phoneNumber}
+        onChange={props.onChange}
+      />
+      <InputTitle>DNI</InputTitle>
+      <Input name="dni" value={props.dni} onChange={props.onChange} />
+    </FormView>
   );
 };
-
-const Title = styled.h1`
-  text-align: center;
-`;
-
-const InputTitle = styled.div`
-  color: ${helpers.color.secondary}
-  font-size: 0.8rem;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  border: unset;
-  font-size: 1rem;
-  height: 2rem;
-  border-bottom: solid 1px ${helpers.color.secondary};
-  margin-bottom: 1rem;
-  transition: border-bottom-color 500ms;
-  &.invalid {
-    border-bottom-color: ${helpers.color.danger}
-  }
-  &:focus {
-    outline: unset
-    border-bottom-color: ${helpers.color.secondaryLight};
-  }
-`;
