892d2efbb2e60a53e8b36ed830593b49c63d0f2b
Fernando Zavalia <zavaliafernando@gmail.com>
2019-06-18 19:47:37 -0300

    Colors

--- src/helpers/ColorHelpers.ts
+++ src/helpers/ColorHelpers.ts
@@ -2,7 +2,7 @@ export default class ColorHelpers {
   primary = "#9384a9";
   primaryLight = '#d1bdef'
   secondaryLight = '#6738af';
-  secondary = "#422371";
+  secondary = "#3c3c3c";
   secondaryDark = '#211138';
   danger = 'red';
 }
