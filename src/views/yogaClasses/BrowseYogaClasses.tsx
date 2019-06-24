6ca2b686b31342bb69e1ec2aa45d1d8139ff5422
Fernando Zavalia <zavaliafernando@gmail.com>
2019-06-23 22:12:59 -0300

    Filter yoga classes by month

--- src/views/yogaClasses/BrowseYogaClasses.tsx
+++ src/views/yogaClasses/BrowseYogaClasses.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import api from "../../modules/api";
 import { History } from "history";
-import BrowseView from "../../components/BrowseView";
+import BrowseView, { FilterType } from "../../components/BrowseView";
 import { YogaClass } from "../../modules/api/requests/YogaClassRequest";
 import helpers from "../../helpers";
 import { OrderType } from "../../modules/api/core/QueryStringBuilder";
+import { PaginatedListOptions } from "../../modules/api/impl/ApiModelRequest";
 
 export default (props: { history: History }) => {
   const date = (yogaClass: YogaClass) =>
@@ -13,21 +14,31 @@ export default (props: { history: History }) => {
   return (
     <BrowseView
       title="Clases"
-      mapItem={yogaClass => ({
-        title: date(yogaClass),
-        props: [{ label: "Asistencias", value: yogaClass.students.length }]
-      })}
-      loadMore={page =>
-        api.yogaClass.paginatedList(page, {
-          include: ["students"],
-          order: { by: "date", type: OrderType.DESC }
-        })
-      }
       history={props.history}
       createItemPath={`/yoga_classes/create`}
       updateItemPath={yogaClass => `/yoga_classes/update/${yogaClass.id}`}
       deletePromise={yogaClass => api.yogaClass.delete(yogaClass.id)}
       deleteMessage={yogaClass => "Eliminar Clase" + date(yogaClass)}
+      mapItem={yogaClass => ({
+        title: date(yogaClass),
+        props: [{ label: "Asistencias", value: yogaClass.students.length }]
+      })}
+      loadMore={(page, filters) => {
+        const options: PaginatedListOptions = {
+          include: ["students"],
+          order: { by: "date", type: OrderType.DESC }
+        };
+        if (filters && filters.month) {
+          const dateRange = helpers.date.getMonthRange(filters.month);
+          options.whereBetween = {
+            date: { min: dateRange.start, max: dateRange.end }
+          };
+        }
+        return api.yogaClass.paginatedList(page, options);
+      }}
+      filters={[
+        { name: "month", label: "Buscar por Més", type: FilterType.MONTH }
+      ]}
     />
   );
 };
